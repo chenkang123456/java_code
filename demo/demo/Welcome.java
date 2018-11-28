766894b436b406a9d23013f22b4e3ff369a83184
chenkang123456 <2447336527@qq.com>
2018-11-28 22:38:29 +0800

    welcome代码

---
+++ demo/demo/Welcome.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to java!");
+		System.out.println("Welcome to Computer Science!");
+		System.out.println("Programming is fun!");
+        System.out.println(1+2+3+4+5+6+7+8+9);
+        System.out.println(((9.5*4.5)-(2.5*3))/(45.5-3.5));
+	}
+
+}
