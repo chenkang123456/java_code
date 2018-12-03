dca23fbbcff788544f3201f39bf123bef111cd9b
chenkang123456 <2447336527@qq.com>
2018-12-03 23:04:17 +0800

    新建类

---
+++ Array/src/array/Array.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author chenkang
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        /**double radius;
+        double area;
+        radius = 20;
+        area = radius * radius *3.14;
+        System.out.println(area);
+        System.out.println("The area for the circle of radius " + radius + " is " + area);*/
+        double i = 50;
+        double k = i + 50;
+        double j = k + 1;
+        System.out.println("j is " + j + " and k is " + k );
+    }
+    
+}
